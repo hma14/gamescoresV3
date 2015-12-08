d290b8dc9313313e071c2dc462ad1cb8a56a91bd
Henry <henryma14@gmail.com>
2015-12-08 11:54:38 -0800

    initial commit

---
+++ GameScore3/Scripts/app/resource.js
@@ -0,0 +1,12 @@
+﻿'use restrict';
+
+var scoreService = angular.module('score', ['ngResource']);
+scoreService.factory('score', ['$resource', '$http',
+    function ($resource, $http) {
+        //return $resource('http://stindillmrtest.blob.core.windows.net/data/scores.json', {},
+        return $resource('/data/scores.json', {},
+            {
+                query: { method: 'GET', isArray: false }
+            });
+    }
+]);
---
+++ GameScore3/Scripts/app/scoreApp.js
@@ -0,0 +1,38 @@
+﻿'use restrict';
+
+var scoreApp = angular.module('scoreApp', ['score']);
+
+scoreApp.controller('scoreController', ['$scope', 'score', '$exceptionHandler', function ($scope, score, $exceptionHandler) {
+    $scope.players = [];
+    $scope.errors = []
+
+    $scope.init = function () {
+        score.query(function (payload) {
+            $scope.players = payload.scores;
+            angular.forEach($scope.players, function (player) {
+                player.last = player.last.toUpperCase();
+                if (player.score == null)
+                {
+                    player.score = -1;
+                }
+            });
+        },
+        
+        function (error) {
+            $exceptionHandler(error);
+        });
+    };
+
+
+}]);
+
+scoreApp.factory('$exceptionHandler', function ($injector) {
+    return function (exception) {
+        var $rootScope = $injector.get('$rootScope');
+        $rootScope.errors = $rootScope.errors || [];
+        if (exception != null) {
+            $rootScope.errors.push(exception.statusText + exception.status);
+        }
+        console.log($rootScope.errors);
+    }
+})
