a9d39c7d6cf355467e7e0120bfa7b4810ae04c6c
Henry <henryma14@gmail.com>
2015-12-14 08:55:47 -0800

    removed reference of $http

--- GameScoreV3/Scripts/app/resource.js
+++ GameScoreV3/Scripts/app/resource.js
@@ -1,12 +1,12 @@
 ﻿'use restrict';
 
 var scoreService = angular.module('score', ['ngResource']);
-scoreService.factory('score', ['$resource', '$http',
-    function ($resource, $http) {
+scoreService.factory('score', ['$resource',
+    function ($resource) {
         //return $resource('http://stindillmrtest.blob.core.windows.net/data/scores.json', {},
         return $resource('/data/scores.json', {},
             {
                 query: { method: 'GET', isArray: false }
             });
     }
-]);
+]); 
